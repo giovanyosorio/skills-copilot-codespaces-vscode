e19e0010ce3d69fa21142ef8741868525f52003d
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-09-02 22:29:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,2 @@
-// create web server
-
-
-// to run: node comments.js
-
-// load the express module
-var express = require('express');
-
-// create an express app
-var app = express();
-
-// use the express-static middleware
-app.use(express.static(__dirname + '/public'));
-
-// define the first route
-app.get('/comments', function(req, res) {
-  res.send('Hello World!');
-});
-
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+// create web server with express
+const express = require('express');
