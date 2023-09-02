5d3099ba0b9884220f920f30fd375297859d1849
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-09-02 23:09:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,34 @@
-// Create a web server
-// Respond to requests with a page containing a form
-// Respond to POST requests with the contents of the form sent back as plain text
-// Respond to GET requests with '404 Not Found'
-// Respond to other requests with '405 Method Not Allowed'
-// Listen on a port passed in as an argument to the program
+// Create web service
+// This file is used to create web service for comments
 
-var http = require('http');
-var url = require('url');
+// Import express
+const express = require('express');
+const router = express.Router();
 
-var port = process.argv[2];
+// Import comments model
+const Comments = require('../models/comments');
 
-var server = http.createServer(function (request, response) {
-  var parsedUrl = url.parse(request.url, true);
-  if (parsedUrl.pathname === '/api/parsetime') {
-    var date = new Date(parsedUrl.query.iso);
-    var json = {
-      'hour': date.getHours(),
-      'minute': date.getMinutes(),
-      'second': date.getSeconds()
-    };
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(json));
-  }
-  else if (parsedUrl.pathname === '/api/unixtime') {
-    var date = new Date(parsedUrl.query.iso);
-    var json = {
-      'unixtime': date.getTime()
-    };
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(json));
-  }
-  else {
-    response.writeHead(404);
-    response.end();
-  }
-});
-server.listen(port);
+// Import auth
+const auth = require('../middleware/auth');
+
+// Import comments controller
+const CommentsController = require('../controllers/comments');
+
+// Create web service
+// Create comment
+router.post('/comment', auth, CommentsController.createComment);
+
+// Get all comments
+router.get('/comments', auth, CommentsController.getComments);
+
+// Get comment by id
+router.get('/comment/:id', auth, CommentsController.getCommentById);
+
+// Update comment
+router.patch('/comment/:id', auth, CommentsController.updateComment);
+
+// Delete comment
+router.delete('/comment/:id', auth, CommentsController.deleteComment);
+
+// Export module
+module.exports = router;
